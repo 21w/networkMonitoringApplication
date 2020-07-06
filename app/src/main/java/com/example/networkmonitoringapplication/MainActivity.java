dd03d1dc209d9b47a13b6f56100319826a17902f
21w <akimoro1603401@gmail.com>
2020-07-06 16:06:49 +0900

    Create:NetworkManager File

--- app/src/main/java/com/example/networkmonitoringapplication/MainActivity.java
+++ app/src/main/java/com/example/networkmonitoringapplication/MainActivity.java
@@ -9,26 +9,30 @@ import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.example.networkMonitoringApplication.MESSAGE";
+//    【未使用】public static final String EXTRA_MESSAGE = "com.example.networkMonitoringApplication.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+//    WiFi検索ボタンを押したときの処理
+    public void searchWiFi(View view){
 
-//    送信ボタンを押したときの処理
-    public void sendMessage(View view){
-//        インテント：異なるコンポーネント間で、オブジェクトの参照を出来るようにするためのオブジェクト
-//        インテントの生成（第二引数というActivityを起動させる）
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-//        IDからビューを見つける（ここではEditTextビューを見つけてる）
-        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
-//        EditTextビューから入力された文字列を取得する
-        String message = editText.getText().toString();
-//        第一引数（キー）の値を第二引数（値）として、インテントに追加する
-        intent.putExtra(EXTRA_MESSAGE,message);
-//        インテントを実行する
-        startActivity(intent);
     }
+
+////    【未使用】送信ボタンを押したときの処理
+//    public void sendMessage(View view){
+////        インテント：異なるコンポーネント間で、オブジェクトの参照を出来るようにするためのオブジェクト
+////        インテントの生成（第二引数というActivityを起動させる）
+//        Intent intent = new Intent(this, DisplayMessageActivity.class);
+////        IDからビューを見つける（ここではEditTextビューを見つけてる）
+//        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+////        EditTextビューから入力された文字列を取得する
+//        String message = editText.getText().toString();
+////        第一引数（キー）の値を第二引数（値）として、インテントに追加する
+//        intent.putExtra(EXTRA_MESSAGE,message);
+////        インテントを実行する
+//        startActivity(intent);
+//    }
 }
---
+++ app/src/main/java/com/example/networkmonitoringapplication/NetworkManager.java
@@ -0,0 +1,4 @@
+package com.example.networkmonitoringapplication;
+
+public class NetworkManager {
+}
