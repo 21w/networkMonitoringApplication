0e781f96af2a1b8f8b8803a19bd916355adce9c5
21w <akimoro1603401@gmail.com>
2020-07-07 00:25:10 +0900

    add and update:SSIDの取得と一覧表示の実装

--- app/src/main/java/com/example/networkmonitoringapplication/MainActivity.java
+++ app/src/main/java/com/example/networkmonitoringapplication/MainActivity.java
@@ -2,23 +2,59 @@ package com.example.networkmonitoringapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
-//    【未使用】public static final String EXTRA_MESSAGE = "com.example.networkMonitoringApplication.MESSAGE";
-
+//    アクセス権限コード
+    private final int PERMISSION_CODE_ACCESS_COARSE_LOCATION = 0;
+//    アプリ起動時に実行
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-//    WiFi検索ボタンを押したときの処理
-    public void searchWiFi(View view){
-
+//        権限承諾の確認
+//        ビルドバーションがAndroid6.0以上か
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+//            位置情報へのアクセスを許可されているか
+            if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+//                許可されていないのであれば実行
+//                位置情報へのアクセスをリクエスト
+                this.requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},PERMISSION_CODE_ACCESS_COARSE_LOCATION);
+                return;
+            }
+        }
+//        ネットワークマネージャーを初期化
+        NetworkManager networkManager = new NetworkManager(this);
+//        アクセスポイント一覧を取得
+        List<ScanResult> scanResultList = networkManager.getWiFiAccessPointsList();
+//        SSIDリスト
+        List<String> ssidList = new ArrayList<String>();
+//        アクセスポイント一覧からSSIDをリスト化する
+        for (ScanResult accessPoint : scanResultList){
+            ssidList.add(accessPoint.SSID);
+        }
+//        アダプターの生成
+        ArrayAdapter ssidAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, ssidList);
+//        リストを取得
+        ListView wifiList = (ListView)findViewById(R.id.wifiList);
+//        アダプターをリストに接続
+        wifiList.setAdapter(ssidAdapter);
     }
 
 ////    【未使用】送信ボタンを押したときの処理
--- app/src/main/java/com/example/networkmonitoringapplication/NetworkManager.java
+++ app/src/main/java/com/example/networkmonitoringapplication/NetworkManager.java
@@ -1,4 +1,21 @@
 package com.example.networkmonitoringapplication;
 
-public class NetworkManager {
+import android.content.Context;
+import android.net.wifi.WifiManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
+
+public class NetworkManager extends AppCompatActivity {
+//    WiFiマネージャーの定義
+    public WifiManager wifiManager;
+    public NetworkManager (Context context){
+//        WiFiマネージャーの初期化
+        wifiManager = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+    }
+//    アクセスポイント一覧を取得
+    public List getWiFiAccessPointsList(){
+        return wifiManager.getScanResults();
+    }
 }
